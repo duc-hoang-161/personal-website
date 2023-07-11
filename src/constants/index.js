d340af3a7feed280b534f6840c138e7bcd7c2c04
Duc Hoang <31062901+duc-hoang-161@users.noreply.github.com>
2023-07-11 17:31:40 -0400

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -360,28 +360,28 @@ export const experiences = [
         logo: ces,
         link: 'https://www.codeenginestudio.com/',
         positions: [
-            {
-                title: 'Software Developer (Remote)',
-                duration: 'Aug 2022 - Feb 2023',
-                content: [
-                    {
-                        text: 'Designed technical solutions that aligned with business goals, customer requirements, and industry best practices.',
-                        link: ''
-                    },
-                    {
-                        text: 'Collaborated with the development team to code reviews and provided constructive feedback to team members to ensure code quality, maintainability, and scalability.',
-                        link: ''
-                    },
-                    {
-                        text: 'Provided technical solutions that align with business goals, customer requirements, and industry best practices.',
-                        link: ''
-                    },
-                    {
-                        text: 'Conducted feasibility studies to evaluate the viability of implementing new technologies and platforms in existing systems.',
-                        link: ''
-                    }
-                ],
-            },
+            // {
+            //     title: 'Software Developer (Remote)',
+            //     duration: 'Aug 2022 - Feb 2023',
+            //     content: [
+            //         {
+            //             text: 'Designed technical solutions that aligned with business goals, customer requirements, and industry best practices.',
+            //             link: ''
+            //         },
+            //         {
+            //             text: 'Collaborated with the development team to code reviews and provided constructive feedback to team members to ensure code quality, maintainability, and scalability.',
+            //             link: ''
+            //         },
+            //         {
+            //             text: 'Provided technical solutions that align with business goals, customer requirements, and industry best practices.',
+            //             link: ''
+            //         },
+            //         {
+            //             text: 'Conducted feasibility studies to evaluate the viability of implementing new technologies and platforms in existing systems.',
+            //             link: ''
+            //         }
+            //     ],
+            // },
             {
                 title: 'Software Developer',
                 duration: 'Feb 2020 - Jul 2022',
